b65368e0c0cc31e1614618cb5421b6542503a44a
Scarlet Alkeyo <scarletalkeyo@gmail.com>
2024-03-21 17:03:36 +0300

    Added files

---
+++ arrays.js
@@ -0,0 +1,43 @@
+// finding the last element of the array
+
+
+let arra1 = [3,7,34,90,12]
+console.log('lastitem',arra1.slice(-1));
+
+let arr2 = [true,"green","Where",12,56]
+console.log('lastitem',arr2.slice(-1))
+
+// using an array of array of length 10 insert tomatoe at the 5th index
+
+let fruits = ["Mango","Orange","Banana","Apple","Pear","pawpaw","Watermelon","Avocado","Guavas","Lemon"]
+fruits[5] = "Tomatoes"
+console.log({fruits})
+
+
+//Write a program that sort the following word
+
+let word = "renniw"
+let newWord = word.split("").sort().join("");
+console.log({newWord})
+
+
+
+// write a program that  join array elements in to a string
+
+// myPets = ["Cow","Bird","Snake","Dog"]
+
+
+
+
+
+// A program that duplicates,and console another array that only contain the duplicate
+// let arr = ["boy","man","girl",  "school","girl","woman"]
+  
+
+
+
+
+// Ap program that sort hte following array
+let arr3 = [-5,9,5,3,2,-3,6,8,4,1]
+arr3.sort();
+console.log({arr3})
